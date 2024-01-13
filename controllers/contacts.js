813e39f87fd98239ff78e5fe650d0786dd398ac9
jucinaldo.duarte <jucinaldo.duarte@gmail.com>
2024-01-13 15:43:17 -0300

    deleted:    controllers/contact.js renamed:    controllers/users.js -> controllers/contacts.js renamed:    routes/users.js -> routes/contacts.js modified:   routes/index.js

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,23 +1,20 @@
 const mongodb = require('../data/database');
-const ObjectId = require('mongodb').ObjectId;
+const ObjectId = require(`mongodb`).ObjectId;
 
 const getAll = async (req, res) => {  
     try {
-        const result = await mongodb.getDatabase().db().collection('users').find().toArray();
+        const result = await mongodb.getDatabase().collection('contacts').find().toArray(); 
         res.setHeader('Content-type', 'application/json');
         res.status(200).json(result);
-        
     } catch (error) {
         res.status(500).json({ error: error.message });
-        
     }
-   
 };
 
 const getSingle = async (req, res) => {
     try {
         const userId = new ObjectId(req.params.id);        
-        const result = await mongodb.getDatabase().db().collection('users').findOne({ _id: userId });
+        const result = await mongodb.getDatabase().db().collection('contacts').findOne({ _id: userId });
         res.setHeader('Content-type', 'application/json');
         res.status(200).json(result);
     } catch (error) {
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => {res.send("Hello World");});
 
-router.get('/users', require('./users'));
+router.get('/contacts', require('./contacts'));
 
 module.exports = router;
